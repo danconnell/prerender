9f49392d87778a142ab4819f59f45a23af0a1e70
DC <dan212@gmail.com>
2018-01-11 09:49:26 -0500

    Add whitelist

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(prerender.whitelist());
 
 server.start();
